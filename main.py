f5ec1f4dc3b63354a916efca987ee8e8f4e1fa09
19dlam92 <19dlam92@gmail.com>
2023-02-07 18:22:33 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+suits = ['spades', 'clubs', 'hearts', 'diamonds']
+suit = suits[2]
+rank = 'K'
+value = 10
+
+
+print('Your card is : ' + rank + ' of ' + suit)
+for suit in suits:
+    print(suit)
+
+
+
+
+
+
+    
