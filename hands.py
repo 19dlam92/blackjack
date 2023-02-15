245ad7c935c4118cc82e50945814012bcedb28df
19dlam92 <19dlam92@gmail.com>
2023-02-15 16:44:43 -0500

    working on modularization

--- hands.py
+++ hands.py
@@ -1,5 +1,10 @@
 
 
 class Hand:
-    def __init__(self):
-        
+    def __init__(self, dealer = False):
+        self.cards = []
+        self.value = 0
+        self.dealer = dealer
+
+    def add_card(self, card_list):
+        self.cards.extend(card_list)
